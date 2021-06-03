00b86c6cd882597f58026aae6f74b3c4f9f334a3
saxenatanya <82470912+saxenatanya@users.noreply.github.com>
2021-06-03 16:18:44 -0400

    Create Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js

---
+++ 8-kyu/Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,22 @@
+/*
+
+
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
+
+
+
+function remove(s){
+  //coding and coding....
+  return s.replace(/!+$/,"");
+  
+}
