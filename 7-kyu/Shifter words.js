8d5a1b03ed2531b966b04fd3c2c597e90ae746ad
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2021-10-13 14:48:47 -0400

    Create Shifter words.js

---
+++ 7-kyu/Shifter words.js
@@ -0,0 +1,6 @@
+function shifter(s){
+  // your code here
+  let letters =["H", "I", "N", "O", "S", "X", "Z", "M" ,"W"];
+  let sp =s.split(' ').map((val) => val.map(v1) => v1.contains(letters) ? count++ : null).join('');
+  console.log(sp);
+}
