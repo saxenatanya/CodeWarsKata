0ba92d616630acda761fc06463884530c82e6908
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2021-12-18 00:27:43 +0530

    Create What is between?.js

---
+++ 8-kyu/What is between?.js
@@ -0,0 +1,21 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+function between(a, b) {
+  // your code here
+  let arr=[];
+  for(let i=a;i<=b;i++){
+    arr.push(i)
+  }
+  return arr;
+}
+
+describe("Basic tests", () => {
+  it("between(1, 4)", () => assert.deepStrictEqual(between(1, 4), [1, 2, 3, 4]));
+  it("between(-2, 2)", () => assert.deepStrictEqual(between(-2, 2), [-2, -1, 0, 1, 2]));
+});
