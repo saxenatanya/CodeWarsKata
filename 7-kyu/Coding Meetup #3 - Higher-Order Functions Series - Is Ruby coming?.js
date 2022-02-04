9124ae3372afe43cef8a16b1a7c72cd6032cf592
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2022-02-04 23:11:47 +0530

    Create Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?.js

---
+++ 7-kyu/Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?.js
@@ -0,0 +1,41 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if at least one Ruby developer has signed up; or
+// false if there will be no Ruby developers.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+//   { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+//   { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+// ];
+// your function should return true.
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+
+function isRubyComing(list) {
+  // thank you for checking out my kata :)
+  return list.some((val) => val.language === 'Ruby');
+}
+
+describe("Tests", () => {
+  it("test", () => {
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+var list2 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' } 
+];
+
+Test.assertEquals(isRubyComing(list1), true);
+Test.assertEquals(isRubyComing(list2), false);
+  });
+});
